b5c475f7c29655f48b39bdd48ec5de998f2a7014
Diego <diegofrabasa@gmail.com>
2021-02-03 15:19:35 -0600

    casi final

--- server/node/server.js
+++ server/node/server.js
@@ -3,30 +3,70 @@ const app = express();
 const mercadopago = require("mercadopago");
 
 //REPLACE WITH YOUR ACCESS TOKEN AVAILABLE IN: https://developers.mercadopago.com/panel/credentials
-mercadopago.configurations.setAccessToken("YOUR_ACCESS_TOKEN"); 
+mercadopago.configure({
+	access_token: 'APP_USR-1025205870342941-020219-842d8de4a96b7a1c617c5945d7756787-710045954',
+	integrator_id: 'dev_24c65fb163bf11ea96500242ac130004'
+});
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(express.static("../../client"));
 
 app.get("/", function (req, res) {
-  res.status(200).sendFile("index.html");
+  	res.status(200).sendFile("index.html");
 }); 
 
 app.post("/create_preference", (req, res) => {
 
 	let preference = {
 		items: [{
+			id:1234,
+			"description":"Dispositivo móvil de Tienda e-commerce",
+			"currency_id": "MXN",
+			"picture_url": "https://www.mercadopago.com/org-img/MP3/home/logomp3.gif",
 			title: req.body.description,
 			unit_price: Number(req.body.price),
 			quantity: Number(req.body.quantity),
 		}],
+		"payer": {
+			"name": "​Lalo",
+			"surname": "Landa",
+			"email": "test_user_81131286@testuser.com",
+			"phone": {
+				"area_code": "52",
+				"number": 5549737300
+			},
+			"identification": {
+				"type": "DNI",
+				"number": "12345678"
+			},
+			"address": {
+				"street_name": "Insurgentes Sur",
+				"street_number": 1602,
+				"zip_code": "03940"
+			}
+		},
 		back_urls: {
-			"success": "http://localhost:8080/feedback",
-			"failure": "http://localhost:8080/feedback",
-			"pending": "http://localhost:8080/feedback"
+			"success": "/feedback",
+			"failure": "/feedback",
+			"pending": "/feedback"
 		},
 		auto_return: 'approved',
+		payment_methods: {
+			excluded_payment_methods: [
+				{
+					"id": "amex"
+				}
+			],
+			excluded_payment_types: [
+				{
+					"id": "atm"
+				}
+			],
+			installments: 6
+		},
+		"external_reference": "diegofrabasa@gmail.com",
+		"notification_url": "https://hookb.in/K3GGwEmwx9F0zzW3VJxn"
 	};
 
 	mercadopago.preferences.create(preference)
@@ -39,9 +79,13 @@ app.post("/create_preference", (req, res) => {
 
 app.get('/feedback', function(request, response) {
 	 response.json({
-		Payment: request.query.payment_id,
+		Payment_Id: request.query.payment_id,
+		Collection_Id: request.query.collection_id,
 		Status: request.query.status,
-		MerchantOrder: request.query.merchant_order_id
+		Payment_Method_Id: request.query.payment_type,
+		External_Reference: request.query.external_reference,
+		Merchant_Order_Id: request.query.merchant_order_id,
+		Preference_Id: request.query.preference_id,
 	})
 });
 
