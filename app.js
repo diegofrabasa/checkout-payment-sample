e7576671c56cf55bbc864e7d3483d943b8327d40
Diego <diegofrabasa@gmail.com>
2021-02-03 19:25:34 -0600

    links finales y funcionales

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const port = process.env.PORT || 3000;
 
 //REPLACE WITH YOUR ACCESS TOKEN AVAILABLE IN: https://developers.mercadopago.com/panel/credentials
 mercadopago.configure({
-	access_token: 'APP_USR-1025205870342941-020219-842d8de4a96b7a1c617c5945d7756787-710045954',
+	access_token: 'APP_USR-1159009372558727-072921-8d0b9980c7494985a5abd19fbe921a3d-617633181',
 	integrator_id: 'dev_24c65fb163bf11ea96500242ac130004'
 });
 
